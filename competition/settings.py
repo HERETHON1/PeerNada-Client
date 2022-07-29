0a05e00b8ff7f3ed27eda91ed2be0d87910815de
bchoi2021 <0507sally@gmail.com>
2022-07-29 15:31:54 +0900

    first commit

--- competition/settings.py
+++ competition/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j^vx#*g!9dna+h=u0u4b59w9+2lkko7dc55y0mr(w(4lp5)w*w'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
